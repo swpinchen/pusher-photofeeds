580f72eb9715449c7ec422f4e33b5c8b9bac2fff
Stuart Pinchen <s_pinchen@hotmail.com>
2021-05-27 09:52:32 +0900

    Updated controller to upload to cloudinary

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -1,10 +1,25 @@
 # app/controllers/photo_controller.rb
 class PhotoController < ApplicationController
-  def store
-    render plain: params[:caption].inspect
+  def index
+    @posts = Post.all.order("created_at DESC")
   end
 
-  def index
-    @posts = Post.all
+  def store
+    # upload image to cloudinary
+    @value = Cloudinary::Uploader.upload(params[:image])
+    # create a new post object and save to db
+    @post = Post.new({link: @value['secure_url'], caption: params[:caption]})
+    @post.save
+    # trigger an event with pusher
+    # [...]
+    redirect_to('/')
   end
 end
+
+# def store
+#   render plain: params[:caption].inspect
+# end
+
+# def index
+#   @posts = Post.all
+# end
