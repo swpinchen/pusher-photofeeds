de68acb838370d07580badae19e86f994bab0d80
Stuart Pinchen <s_pinchen@hotmail.com>
2021-05-27 09:38:04 +0900

    Created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
-Rails.application.routes.draw do
-  get 'photo/index'
-  get 'photo/store'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+    # config/routes.rb
+    Rails.application.routes.draw do
+      post 'photo/store'
+      root 'photo#index'
+    end
+
